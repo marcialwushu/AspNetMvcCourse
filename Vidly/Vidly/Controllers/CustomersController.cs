dc8aaa751f61fd813db25747766b011f407d4e29
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-09-03 11:08:38 -0300

    ApplicationUser with IdentityUser and ClaimsIdentity add

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -44,6 +44,7 @@ namespace Vidly.Controllers
             return View("CustomerForm", viewModel);
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)

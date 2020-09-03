54482d056f918d996cce00d6de49bdfa7a6a17ae
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-09-03 17:17:15 -0300

    ManageController initial process
    fixing reference errors

---
+++ Vidly/Vidly/Controllers/ManageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Vidly.Controllers
+{
+    public class ManageController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ManageController()
+        {
+
+        }
+
+        
+
+
+}
+
+    internal class ApplicationUserManager 
+    {
+    }
+
+    internal class ApplicationSignInManager
+    {
+    }
+}

5943f4be08188e1a7814960ce3ca0d329d210e81
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-09-10 11:40:34 -0300

    identityConfig and ManagerController init

---
+++ Vidly/App_Start/IdentityConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            return Task.FromResult(0);
+        }
+    }
+
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+
+            return manager;
+        }
+    }
+}
---
+++ Vidly/Controllers/ManageController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class ManageController : Controller
+    {
+        private AppllicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        
+        public ManageController()
+        {
+
+        }
+
+        public ManageController(AppllicationSignInManager signInManager, ApplicationUserManager userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public class ApplicationUserManager
+        {
+            
+        }
+
+        public class AppllicationSignInManager 
+        {
+            
+        }
+    }
+
+    
+}

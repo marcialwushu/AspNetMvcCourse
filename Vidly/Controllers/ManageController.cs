9d9908659f7148ea8831d9bf8c4b9fe7225665db
Cleilson Sousa Pereira <cleilson.pereira@fitbank.com.br>
2020-09-10 15:07:12 -0300

    AddPhoneNumber and RemoveLogin methods added in ManageController

--- Vidly/Controllers/ManageController.cs
+++ Vidly/Controllers/ManageController.cs
@@ -10,6 +10,9 @@ using System.Web.Mvc;
 using Microsoft.Owin.Security;
 using Owin;
 using Vidly.App_Start;
+using System.Threading.Tasks;
+using Vidly.Models;
+using System.Net;
 
 namespace Vidly.Controllers
 {
@@ -25,7 +28,7 @@ namespace Vidly.Controllers
 
         public ManageController(ApplicationSignInManager signInManager, ApplicationUserManager userManager)
         {
-            _signInManager = signInManager;
+            SignInManager = signInManager;
             UserManager = userManager;
         }
 
@@ -51,6 +54,100 @@ namespace Vidly.Controllers
             {
                 _signInManager = value;
             }
+        }
+
+
+        // GET: /Manage/Index
+        public async Task<ActionResult> Index(ManageMessageId? message)
+        {
+            ViewBag.StatusMessage =
+                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed"
+                : message == ManageMessageId.SetPasswordSuccess ? "Your password has bee set"
+                : message == ManageMessageId.SetTwoFactorSuccess ? "Yout two-factor authenticator provider has been set"
+                : message == ManageMessageId.Error ? "An error has occured."
+                : message == ManageMessageId.AddPhoneSuccess ? "Your phone number added"
+                : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed"
+                : "";
+
+            var userId = User.Identity.GetUserId();
+            var model = new IndexViewModel
+            {
+                HasPassword = HasPassword(),
+                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
+                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
+                Logins = await UserManager.GetLoginsAsync(userId),
+                
+
+            };
+            return View(model);
+        }
+
+        public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
+        {
+            ManageMessageId? message;
+            var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if(user != null)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                }
+                message = ManageMessageId.RemoveLoginSuccess;
+            }
+            else
+            {
+                message = ManageMessageId.Error;
+            }
+            return RedirectToAction("ManageLogins", new { Message = message });
+
+        }
+
+        // GET: /Manage/AddPhoneNumber
+        public ActionResult AddPhoneNumber()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> AddPhoneNumber(AddPhoneNumberViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Generate the token and send it
+            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);
+            if(UserManager.SmsService != null)
+            {
+                var message = new IdentityMessage
+                {
+                    Destination = model.Number,
+                    Body = "Your security code is: " + code
+                };
+                await UserManager.SmsService.SendAsync(message);
+            }
+            return RedirectToAction("VerifyPhoneNumber", new { PhoneNumber = model.Number });
+
+        }
+
+        private bool HasPassword()
+        {
+            
+            
+            return false;
+        }
+
+        
+
+        public enum ManageMessageId
+        {
+            AddPhoneSuccess,
+            ChangePasswordSuccess,
+            SetTwoFactorSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+            RemovePhoneSuccess,
+            Error
         }
     }
 
